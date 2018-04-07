6e9d3ae4283f84fd9256c2925ea5d80a4dde2ad9
Paul <pvdstel@users.noreply.github.com>
2018-04-08 00:03:50 +0200

    Add project files

---
+++ src/bfng/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace bfng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
