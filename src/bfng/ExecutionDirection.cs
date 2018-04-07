9b6277482ca95f70ba7c428d5b141e52b7e2f64a
Paul <pvdstel@users.noreply.github.com>
2018-04-08 01:23:58 +0200

    Add class skeletons

---
+++ src/bfng/ExecutionDirection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bfng
+{
+    public enum ExecutionDirection
+    {
+        Right,
+        Left,
+        Up,
+        Down
+    }
+}
---
+++ src/bfng/Parser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bfng
+{
+    public class Parser
+    {
+
+    }
+}
